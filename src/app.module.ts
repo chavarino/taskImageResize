bb1973db3a014c0db7232e2681eab439d66e57e5
javier <javier.chavarino.1991@gmail.com>
2025-08-20 17:40:59 +0200

    renaming element

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CreateTaskUseCase } from './domain/use-cases/create-task.use-case/create-task.use-case';
 import { GetTaskUseCase } from './domain/use-cases/get-task.use-case/get-task.use-case';
 import { TasksController } from './infrastructure/http/controllers/tasks/tasks.controller';
-import { MockTaskRepository } from './infrastructure/persistence/mongo-task.repository/mock-task.repository.service';
+import { MockTaskRepository } from './infrastructure/persistence/mock-task.repository/mock-task.repository.service';
 import { BaseTaskRepository } from './infrastructure/persistence/base-task.repository.service';
 import { GenerateVariantsUseCase } from './domain/use-cases/generate-variants.use-case';
 import { FsOutputWriterService } from './infrastructure/filesystem/fs-output-writer.service';
