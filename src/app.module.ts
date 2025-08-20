b95fd2d08defc2f3dcebffe551e5bf6a9928c62e
javier <javier.chavarino.1991@gmail.com>
2025-08-20 19:00:23 +0200

    changing the import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { CreateTaskUseCase } from './domain/use-cases/create-task.use-case/create-task.use-case';
-import { GenerateVariantsUseCase } from './domain/use-cases/generate-variants.use-case';
 import { GetTaskUseCase } from './domain/use-cases/get-task.use-case/get-task.use-case';
-import { FsOutputWriterService } from './infrastructure/filesystem/fs-output-writer.service';
 import { TasksController } from './infrastructure/http/controllers/tasks/tasks.controller';
-import { ImageResizerService } from './infrastructure/images/image-resizer.service';
-import { InputReaderFactory } from './infrastructure/input/input-reader.factory';
-import { FsInputReaderStrategy } from './infrastructure/input/strategies/fs-input-reader.strategy';
-import { HttpInputReaderStrategy } from './infrastructure/input/strategies/http-input-reader.strategy';
 import { PersistenceModule } from './infrastructure/persistence/persistence.module';
-import { ImageQueueModule } from './infrastructure/queues/bullmq/image-queue.module';
-import { FileNamingService } from './shared/utils/file-naming.service';
-import { HashingService } from './shared/utils/hashing.service';
-import { InputTypeService } from './shared/utils/input-type.service';
 import { ImageQueueConsumerModule } from './infrastructure/queues/bullmq/image-queue.consumer.module';
+import { ImageQueueModule } from './infrastructure/queues/bullmq/image-queue.module';
 const CONSUMER_ENABLED =
   String(process.env.CONSUMER ?? 'false').toLowerCase() === 'true';
 
@@ -29,23 +19,6 @@ const configImports = () => {
 @Module({
   imports: configImports(),
   controllers: [TasksController],
-  providers: [
-    CreateTaskUseCase,
-    GetTaskUseCase,
-
-    // Utils/infra para generate variants
-    ImageResizerService,
-    FileNamingService,
-    HashingService,
-
-    // Input resolution (FS/HTTP a demanda)
-    InputTypeService,
-    FsInputReaderStrategy,
-    HttpInputReaderStrategy,
-    InputReaderFactory,
-
-    // Output writer (FS bajo ./output)
-    { provide: FsOutputWriterService, useClass: FsOutputWriterService },
-  ],
+  providers: [CreateTaskUseCase, GetTaskUseCase],
 })
 export class AppModule {}
--- src/infrastructure/queues/bullmq/image-queue.consumer.module.ts
+++ src/infrastructure/queues/bullmq/image-queue.consumer.module.ts
@@ -4,6 +4,14 @@ import { BullModule } from '@nestjs/bullmq';
 import { IMAGE_QUEUE } from './image-queue.constants';
 import { ImageVariantsProcessor } from './image-variants.processor';
 import { GenerateVariantsUseCase } from 'src/domain/use-cases/generate-variants.use-case';
+import { FsOutputWriterService } from 'src/infrastructure/filesystem/fs-output-writer.service';
+import { ImageResizerService } from 'src/infrastructure/images/image-resizer.service';
+import { InputReaderFactory } from 'src/infrastructure/input/input-reader.factory';
+import { FsInputReaderStrategy } from 'src/infrastructure/input/strategies/fs-input-reader.strategy';
+import { HttpInputReaderStrategy } from 'src/infrastructure/input/strategies/http-input-reader.strategy';
+import { FileNamingService } from 'src/shared/utils/file-naming.service';
+import { HashingService } from 'src/shared/utils/hashing.service';
+import { InputTypeService } from 'src/shared/utils/input-type.service';
 
 @Module({
   imports: [
@@ -11,6 +19,22 @@ import { GenerateVariantsUseCase } from 'src/domain/use-cases/generate-variants.
       name: IMAGE_QUEUE,
     }),
   ],
-  providers: [ImageVariantsProcessor, GenerateVariantsUseCase],
+  providers: [
+    // Utils/infra para generate variants
+    ImageResizerService,
+    FileNamingService,
+    HashingService,
+
+    // Input resolution (FS/HTTP a demanda)
+    InputTypeService,
+    FsInputReaderStrategy,
+    HttpInputReaderStrategy,
+    InputReaderFactory,
+
+    // Output writer (FS bajo ./output)
+    { provide: FsOutputWriterService, useClass: FsOutputWriterService },
+    ImageVariantsProcessor,
+    GenerateVariantsUseCase,
+  ],
 })
 export class ImageQueueConsumerModule {}
